d28d97ea0c7138e8baf254b4c52f2ee5673c4dc2
bit-flipping-alejo <acaicedo@csumb.edu>
2019-08-07 18:09:54 -0700

    fix overwritten code

--- app.js
+++ app.js
@@ -17,68 +17,74 @@ const mc_tools = require("./mc_tools.js");
 //    Alejandro Server Routes
 //------------------------------------
 
-app.use( session({
+app.use(session({
     secret: "top secret!",
     resave: true,
     saveUninitialized: true
 }))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
+    req.session.authenticated = false;
     res.render("index");
 });
 
-app.post("/ac_login", async function(req, resp){
-    //var dbConn = ac_tools.createSqlDb_connection();
-    //var sqlQuery = ac_tools.get_isValidUser_SQL();
-    //var sqlParams = [req.body.ac_username];
-    //var sqlResults = ac_tools.sendQuery_getResults(sqlQuery, sqlParams);
-    // ---> sqlResults will have isAdmin Bool and the hashed PW
-    // ---> If sqlResults is undefined then user doesnt exist
+app.post("/ac_login", async function (req, resp) {
+    console.log(req.session.authenticated)
+    if (req.session.authenticated == false) {
+        var dbConn = ac_tools.createSqlDb_connection();
+        var sqlQuery = ac_tools.get_isValidUser_SQL();
+        var sqlParams = [req.body.ac_username];
+        var sqlResults = await ac_tools.sendQuery_getResults(sqlQuery, sqlParams);
+        // ---> sqlResults will have isAdmin Bool and the hashed PW
+        // ---> If sqlResults is undefined then user doesnt exist
 
-    //if (typeof sqlResults != "undefined") {
-    //    var authenticated = ac_tools.ac_checkPassword( req.body.ac_pass , sqlResults[0].password );
-    //    var isAdmin = sqlResults[0].adminPriv;
-    //    req.session.authenticated = authenticated;
-    //    req.session.isAdmin = isAdmin;
-    //} else {
-    //    var authenticated = false;   
-    //    var isAdmin = false;         
-    //}
-
+        if (typeof sqlResults != "undefined") {
+            var authenticated = ac_tools.ac_checkPassword(req.body.ac_pass, sqlResults[0].password);
+            var isAdmin = sqlResults[0].adminPriv;
+            req.session.authenticated = authenticated;
+            req.session.isAdmin = isAdmin;
+            req.session.username = req.body.ac_username;
+        } else {
+            var authenticated = false;
+            var isAdmin = false;
+        }
+    }
     //Required authentication bools
-    var authenticated = true;   //replaced with function above
-    var isAdmin = true;         //replaced with function above
-    req.session.username = req.body.ac_username;
-    req.session.authenticated = authenticated;
-    req.session.isAdmin = isAdmin
+    //var authenticated = true;   //replaced with function above
+    //var isAdmin = true;         //replaced with function above
+    //req.session.username = req.body.ac_username;
+    //req.session.authenticated = authenticated;
+    //req.session.isAdmin = isAdmin
 
     if (authenticated) {
 
-    var newsURL = "https://spaceflightnewsapi.net/api/v1/articles";
-    var NASA_apod_url = "https://api.nasa.gov/planetary/apod?api_key=B49OqOPlbI5JvvBHEwimMRvdtBCWEEsdjgb5eepB";
-    var apiData = await ac_tools.sendNewsAPI_request(newsURL);
-    var apodData = await ac_tools.sendAPODapi_request(NASA_apod_url);
+        var newsURL = "https://spaceflightnewsapi.net/api/v1/articles";
+        var NASA_apod_url = "https://api.nasa.gov/planetary/apod?api_key=B49OqOPlbI5JvvBHEwimMRvdtBCWEEsdjgb5eepB";
+        var apiData = await ac_tools.sendNewsAPI_request(newsURL);
+        var apodData = await ac_tools.sendAPODapi_request(NASA_apod_url);
 
-    resp.render("login_page", {"username": req.body.ac_username, 
-                               "titles": apiData.title, 
-                               "urls":urls, 
-                               "imgUrls":apiData.imgUrl, 
-                               "numToDisplay":8,
-                               "apodImgUrl": apodData.apodURL,
-                               "apodTitle": apodData.apodTitle,
-                               "apodCopyright": apodData.apodCopyright,
-                               "isAdmin": isAdmin});
+        resp.render("login_page", {
+            "username": req.body.ac_username,
+            "titles": apiData.title,
+            "urls": urls,
+            "imgUrls": apiData.imgUrl,
+            "numToDisplay": 8,
+            "apodImgUrl": apodData.apodURL,
+            "apodTitle": apodData.apodTitle,
+            "apodCopyright": apodData.apodCopyright,
+            "isAdmin": isAdmin
+        });
 
     } else {
         // not authenticated goes here:
-        resp.render("index", {"loginError":true})
+        resp.render("index", { "loginError": true })
     }
-    
+
 });
 
-app.get("/logout", function(req, res){
+app.get("/logout", function (req, res) {
     console.log("From inside /logout path: User chose to log out");
     res.session.destroy();
     res.redirect("/");
@@ -92,7 +98,7 @@ app.get("/logout", function(req, res){
 //    BEGIN Ivan Admin Page Route
 //------------------------------------
 
-app.get("/adminPage", function (req, res){
+app.get("/adminPage", function (req, res) {
     var conn = ac_tools.createSqlDb_connection();
     var sql = "SELECT * FROM Products";
 
@@ -114,7 +120,7 @@ app.get("/adminPage", function (req, res){
             if (err) throw err;
             conn.query(sql, function (err, results) {
                 if (err) throw err;
-                res.render("adminPage", {"adminName": "ivan", "rows": results});
+                res.render("adminPage", { "adminName": "ivan", "rows": results });
             })
         })
     }
@@ -152,7 +158,7 @@ app.post("/adminPage", function (req, res) {
     var sql = "SELECT * FROM Products";
     conn.query(sql, function (err, results) {
         if (err) throw err;
-        res.render("adminPage", {"adminName": "ivan", "rows": results});
+        res.render("adminPage", { "adminName": "ivan", "rows": results });
     })
 });
 //------------------------------------
@@ -164,29 +170,29 @@ app.post("/adminPage", function (req, res) {
 //------------------------------------
 
 //checkout
-app.get("/mc_checkout", function(req, res) {
+app.get("/mc_checkout", function (req, res) {
 
-  //connect to the sql database
-  //var conn = mc_tools.createConnection();
-  
-  //var sql = "SELECT userID FROM checkout";
-  //res.render("checkout")
+    //connect to the sql database
+    //var conn = mc_tools.createConnection();
+
+    //var sql = "SELECT userID FROM checkout";
+    //res.render("checkout")
 });
 
-app.get("/api/getCheckout", function(req, res) {
-  var conn = mc_tools.checkoutConnction();
-  var sql = "SELECT DISTINCT DetailedTransactions.itemID, Products.itemName, Products.price FROM `Products` INNER JOIN `DetailedTransactions` ON DetailedTransactions.itemID = Products.itemID WHERE DetailedTransactions.transID = ?"
-  var sqlTransID = [req.query.transID];
-  
-  conn.connect( function(err){
-    
-    if (err) throw err;
-    conn.query(sql, sqlTransID, function(err, results) {
-      if (err) throw err;
-      res.send(results);
-      
-    });//query
-  });//connect
+app.get("/api/getCheckout", function (req, res) {
+    var conn = mc_tools.checkoutConnction();
+    var sql = "SELECT DISTINCT DetailedTransactions.itemID, Products.itemName, Products.price FROM `Products` INNER JOIN `DetailedTransactions` ON DetailedTransactions.itemID = Products.itemID WHERE DetailedTransactions.transID = ?"
+    var sqlTransID = [req.query.transID];
+
+    conn.connect(function (err) {
+
+        if (err) throw err;
+        conn.query(sql, sqlTransID, function (err, results) {
+            if (err) throw err;
+            res.send(results);
+
+        });//query
+    });//connect
 });//getCheckout
 
 //------------------------------------
@@ -198,8 +204,14 @@ app.get("/api/getCheckout", function(req, res) {
 //    Server Listeners
 //------------------------------------
 
-// Local Environment (please change port number according to your environment as local or codeanywhere)
-app.listen("8081", "0.0.0.0", function () {
+// heroku uses 0.0.0.0
+/*app.listen("8081", "0.0.0.0", function () {
+    console.log("Express server is running...")
+});
+*/
+
+//local machine uses 127.0.0.1
+app.listen("8081", "127.0.0.1", function () {
     console.log("Express server is running...")
 });
 
