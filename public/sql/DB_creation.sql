67a2914409fdc64303978aad5d92414f216c304f
ialej001 <ivan.alejandre@gmail.com>
2019-08-15 09:07:23 -0700

    finished product table, add corresponding imgs

--- public/sql/DB_creation.sql
+++ public/sql/DB_creation.sql
@@ -44,11 +44,14 @@ CREATE TABLE usercart
 -- users
 insert into users values
 (default, 'admin', '$2b$04$Wappq2OLsTdMcZEUmUjrFeXQCbfgQITrb/vMDG2R02oR3joYer0su', True),
-(default, 'buyer', '$2b$04$9eDgKZebf2bMQaz1BVBSSuVmFFYGS4TAArmJ9viDOsTGUhj3BGq92', False);
+(default, 'buyer', '$2b$04$9eDgKZebf2bMQaz1BVBSSuVmFFYGS4TAArmJ9viDOsTGUhj3BGq92', False),
+(default, 'buyer2', '$2b$04$9eDgKZebf2bMQaz1BVBSSuVmFFYGS4TAArmJ9viDOsTGUhj3BGq92', False),
+(default, 'buyer3', '$2b$04$9eDgKZebf2bMQaz1BVBSSuVmFFYGS4TAArmJ9viDOsTGUhj3BGq92', False);
 
 -- products
 INSERT INTO products VALUES
 -- Stars
+(default, 'Black hole', 15000000000, 'A stellar void of destruction, a black hole is invisible to the eye unless it is feeding on a nearby star. Perfect for those who want to recreate the system from "Interstellar"', 'black, hole'),
 (default, 'Hyper-giants', 10000000000, 'The largest of the largest stars in existence. They are typically blue, yellow, or red in appearance and are the brightest of all stars. However, they are unstable in the fact that they constantly lose mass via stellar winds. Due to this fact, hypergiants have extremely short stellar lives with the minimum being in the range of a couple thousand years.', 'hyper, giant, star'),
 (default, 'Super-giants', 10000000000, 'The boundary between super and hypergiants is basically nonexistent. Stars in this range are on a continuum with supergiants being on the lower end of this continuum. Much of the  features of a hypergiant are present in supergiants and as such, can be just as unstable. Supergiants are less  luminous and massive than hypergiants as they have lost more of their fuel to stellar winds.', 'super, giant, star'),
 (default, 'Giants', 6000000000, 'Giant stars are a stellar evolutionary product from main sequence stars.  The mass of the main sequence star will determine the size of the resulting giant star once the main star burns all of its hydrogen. Sometimes, a giant star will not result from a main sequence star due to insufficient mass.', 'giant, star'),
@@ -56,7 +59,7 @@ INSERT INTO products VALUES
 (default, 'Yellow Giant', 5800000000, 'Defined with intermediate surface temperature, yellows have a shorter lifespan compared to reds due to the fact that they have more mass.', 'yellow, giant, star'),
 (default, 'Blue and White Giants', 5500000000, 'The hottest of the giants and the most unstable in terms of mass, size, and luminosity. These turbulent giants have the shortest lifespan.', 'blue, white, giant, star'),
 (default, 'Main sequence stars', 5000000000, 'These stars have a huge variation in all aspects of a star. They can range from red to blue in hue. The more massive the star, the bluer it is and the shorter its lifespan.','regular, main, star'),
--- dwarfs
+(default, 'White dwarf', 6200000000, 'A star with the mass of a main sequence star compacted into a planet the size of the Earth. Incredibly dense and gravity-strong for its size, this star is fairly dim.', 'dwarf, white, star'),
 -- Planets
 (default, 'Planetars', 4200000000, 'These planets are larger than ‘giant’ planets. They are essentially failed brown dwarf stars. Absolutely massive even in comparison to the biggest gas giant and just out of reach of stellar ignition.', 'huge, giant, planet'),
 (default, '"Giant" planets', 4000000000, 'These planets have a thick atmosphere composed of hydrogen and helium. Typically, these planets do not have a defined core and are more of a gradient due to increasing pressure as you delve deeper into the atmosphere. The mass of giant planets are related by the mass of Jupiter. One Jupiter mass is roughly 320,000 masses of the Earth. There are some subtypes to choose from.', 'giant, planet'),
